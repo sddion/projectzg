bee4009230d8c1806989203bc0413359d58f5843
dedsec <dedsec@leviathan.0xdedsec>
2025-11-16 23:19:47 +0530

    Back to express.js

--- api-backup/lib/supabase.d.ts
+++
@@ -1,2 +0,0 @@
-export declare const supabase: import("@supabase/supabase-js").SupabaseClient<any, "public", "public", any, any>;
-export declare const supabaseAdmin: import("@supabase/supabase-js").SupabaseClient<any, "public", "public", any, any> | null;
